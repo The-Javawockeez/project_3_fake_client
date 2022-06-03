9afa8d00802e3ba94281fce5d93efff22ca6ca18
Paige W <ward.paige96@gmail.com>
2022-06-03 15:18:27 -0600

    Home component

---
+++ src/Home.js
@@ -0,0 +1,7 @@
+const Home = () => {
+    return (
+        <p>The Jabbawockeez is an American hip-hop dance crew that rose to prominence as the winner of the first season of America's Best Dance Crew in 2008. Established in 2003 in San Diego, California when dancers Kevin "KB" Brewer and Joe Larot put together an innovative dance crew.</p>
+    )
+}
+
+export default Home
